9c20691e1d48931f0302671cc4405fa027836e95
bmajor <mbencus89@gmail.com>
2025-01-09 09:58:45 +0100

    will continue with db schema before we progress to pattern implementation with click for cli

--- apps/users/models.py
+++ apps/users/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 
+class ValidRoles(models.TextChoices):
+	USER = 'user', 'User'
+	ADMIN = 'admin', 'Admin'
+	BOT = 'bot', 'BOT'
+
+
 '''
 CREATE TABLE app_users_roles {
 	user_role VARCHAR(10) PRIMARY KEY,
@@ -13,18 +19,18 @@ INSERT INTO app_users_roles(user_role) VALUES
 ('bot');
 '''
 class AppUsersRoles(models.Model):
-
-	'''
-	Validation logic next to the ForeignKey in AppUsers
-	'''
-	VALID_ROLES = ['user', 'admin', 'bot']
-	user_role = models.CharField(max_length=10, primary_key=True) #no specific enum in django tho
+	'''Validation logic next to the ForeignKey in AppUsers'''
+	user_role = models.CharField(
+		max_length=10, 
+		primary_key=True,
+		choices=ValidRoles.choices
+	) #no specific enum in django tho
 
 	class Meta:
 		db_table = "app_users_role" #the explicit name of the table
 	
 	def save(self, *args, **kwargs):
-		if self.user_role not in self.VALID_ROLES:
+		if self.user_role not in ValidRoles:
 			raise ValueError(f"Invalid role: {self.user_role}, user_role must be one of {self.VALID_ROLES}.")
 		super().save(*args, **kwargs)
 	
@@ -54,17 +60,17 @@ class AppUsers(models.Model):
 		related_name="users" #not sure what this does just yet
 	)
 	user_age = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(110)])
-	user_gender = models.CharField(max_length=100, null=True, blank=True)
+	user_gender = models.CharField(max_length=20, null=True, blank=True)
 	created_at = models.DateTimeField(auto_now_add=True)
 
 	class Meta:
 		db_table = "app_users"
 
 	def save(self, *args, **kwargs):
-		if not self.user_name.strip():  # Check for empty or whitespace-only names
+		if not self.user_name.strip():  #check for empty or whitespace only names
 			raise ValueError("User name cannot be empty or whitespace.")
 		super().save(*args, **kwargs)
 
 	def __str__(self):
-		return f"{self.user_name} (ID={self.user_id})" #prob we should go about ids?
+		return f"{self.user_name} (ID={self.user_id})"
 
