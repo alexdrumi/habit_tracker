ff354f41504d61cbb36e16450079806afcce39bd
bmajor <mbencus89@gmail.com>
2025-03-17 12:02:42 +0100

    adding google style docstrings

--- apps/analytics/repositories/analytics_repository.py
+++ apps/analytics/repositories/analytics_repository.py
@@ -34,19 +34,33 @@ def handle_analytics_repository_errors(f):
 
 class AnalyticsRepository:
 	def __init__(self, database: MariadbConnection, habit_repository: HabitRepository):
+		"""
+		Creates an analytics repository. Uses dependency injection for related
+		database and habit repository.
+		"""
 		self._db = database
 		self._habit_repository = habit_repository
 
-
 	@handle_analytics_repository_errors
 	def create_analytics(self, times_completed, streak_length, habit_id, last_completed_at=None):
-		'''
-		Create analytics for a certain analytics.
-		'''
-		#validation of habit will come from the service layer
-		with self._db._connection.cursor() as cursor:
-			#probably validation should happen in the service later
-	
+		"""
+		Creates an analytics record for a specific habit.
+
+		Args:
+			times_completed (int): The number of times the habit has been completed.
+			streak_length (int): The current streak length for the habit.
+			habit_id (int): The unique identifier of the habit.
+			last_completed_at (datetime, optional): The last completion time of the habit. Defaults to None.
+
+		Returns:
+			dict: A dictionary containing the newly created analytics record (including 'analytics_id').
+
+		Raises:
+			IntegrityError: If there's a database integrity issue during creation.
+			AnalyticsRepositoryError: For repository-level errors related to analytics.
+			Exception: For any other unexpected errors.
+		"""
+		with self._db._connection.cursor() as cursor:	
 			query = "INSERT INTO analytics(times_completed, streak_length, last_completed_at, created_at, habit_id_id) VALUES (%s, %s, %s, NOW(), %s);"
 			cursor.execute(query, (times_completed, streak_length, last_completed_at, habit_id))
 			self._db._connection.commit()
@@ -59,8 +73,24 @@ class AnalyticsRepository:
 			}
 
 
+
 	@handle_analytics_repository_errors
 	def get_analytics_id(self, habit_id):
+		"""
+		Retrieves the analytics ID associated with a given habit.
+
+		Args:
+			habit_id (int): The unique identifier of the habit.
+
+		Returns:
+			int: The analytics ID corresponding to the provided habit_id.
+
+		Raises:
+			AnalyticsNotFoundError: If no analytics record is found for the habit.
+			IntegrityError: For database integrity-related errors.
+			AnalyticsRepositoryError: For repository-level analytics errors.
+			Exception: For any other unexpected errors.
+		"""
 		with self._db._connection.cursor() as cursor:
 			query = "SELECT analytics_id FROM analytics WHERE habit_id_id = %s;"
 			cursor.execute(query, (habit_id,))
@@ -72,42 +102,77 @@ class AnalyticsRepository:
 				raise AnalyticsNotFoundError(f"Analytics for habit with habit id: {habit_id} is not found.")
 
 
+
 	@handle_analytics_repository_errors
 	def update_analytics(self, analytics_id, times_completed=None, streak_length=None, last_completed_at=None):
-			updated_fields = []
-			updated_values = []
+		"""
+		Updates an existing analytics record.
 
-			if times_completed is not None:
-				updated_fields.append("times_completed = %s")
-				updated_values.append(times_completed)
-			
-			if streak_length is not None:
-				updated_fields.append("streak_length = %s")
-				updated_values.append(streak_length)
+		Args:
+			analytics_id (int): The unique identifier of the analytics record to be updated.
+			times_completed (int, optional): Updated number of completions. Defaults to None.
+			streak_length (int, optional): Updated streak length. Defaults to None.
+			last_completed_at (datetime, optional): Updated datetime for the last completion. Defaults to None.
 
-			if last_completed_at is not None:
-				updated_fields.append("last_completed_at = %s")
-				updated_values.append(last_completed_at)
-			
-			if not updated_fields:
-				return 0
-			
-			set_commands = ', '.join(updated_fields)
-			updated_values.append(analytics_id)
+		Returns:
+			int: The number of rows affected by the update (typically 1 if successful).
 
-			query = "UPDATE analytics SET " + set_commands + " WHERE analytics_id = %s;"
-			with self._db._connection.cursor() as cursor:
-				cursor.execute(query, updated_values)
-				self._db._connection.commit()
+		Raises:
+			AnalyticsNotFoundError: If the analytics record is not found.
+			IntegrityError: For database integrity-related errors.
+			AnalyticsRepositoryError: For repository-level analytics errors.
+			Exception: For any other unexpected errors.
+		"""
+		updated_fields = []
+		updated_values = []
 
-				if cursor.rowcount == 0: #shouldnt be the case by now
-					raise AnalyticsNotFoundError(f"Analytics for habit with analyticsid: {analytics_id} is not found.")
+		if times_completed is not None:
+			updated_fields.append("times_completed = %s")
+			updated_values.append(times_completed)
+		
+		if streak_length is not None:
+			updated_fields.append("streak_length = %s")
+			updated_values.append(streak_length)
 
-				return cursor.rowcount #nr of rows effected in UPDATE SQL (ideally 1)
+		if last_completed_at is not None:
+			updated_fields.append("last_completed_at = %s")
+			updated_values.append(last_completed_at)
+		
+		if not updated_fields:
+			return 0
+		
+		set_commands = ', '.join(updated_fields)
+		updated_values.append(analytics_id)
+
+		query = "UPDATE analytics SET " + set_commands + " WHERE analytics_id = %s;"
+		with self._db._connection.cursor() as cursor:
+			cursor.execute(query, updated_values)
+			self._db._connection.commit()
+
+			if cursor.rowcount == 0: #shouldnt be the case by now
+				raise AnalyticsNotFoundError(f"Analytics for habit with analyticsid: {analytics_id} is not found.")
+
+			return cursor.rowcount #nr of rows effected in UPDATE SQL (ideally 1)
+
 
 
 	@handle_analytics_repository_errors  
 	def delete_analytics(self, analytics_id):
+		"""
+		Deletes an analytics record.
+
+		Args:
+			analytics_id (int): The unique id of the analytics record to delete.
+
+		Returns:
+			int: The number of rows deleted (1 if successful).
+
+		Raises:
+			AnalyticsNotFoundError: If the analytics record is not found.
+			IntegrityError: For database integrity-related errors.
+			AnalyticsRepositoryError: For other repository-level analytics errors.
+			Exception: For any other unexpected errors.
+		"""
 		with self._db._connection.cursor() as cursor:
 			query = "DELETE FROM analytics WHERE analytics_id = %s"
 			cursor.execute(query, (analytics_id,))
@@ -120,6 +185,18 @@ class AnalyticsRepository:
   
 	@handle_analytics_repository_errors
 	def calculate_longest_streak(self):
+		"""
+		Retrieves the habit that has the maximum current streak in the database.
+
+		Returns:
+			tuple: A tuple containing (habit_id, habit_name, habit_streak) for the habit with the longest streak.
+
+		Raises:
+			AnalyticsNotFoundError: If no habit streaks are found.
+			IntegrityError: For database integrity-related errors.
+			AnalyticsRepositoryError: For other repository-level analytics errors.
+			Exception: For any other unexpected errors.
+		"""
 		with self._db._connection.cursor() as cursor:
 			query = "SELECT habit_id, habit_name, habit_streak FROM habits WHERE habit_streak = (SELECT MAX(habit_streak) FROM habits) ORDER BY habit_streak DESC;"
 			
@@ -132,8 +209,24 @@ class AnalyticsRepository:
 				raise AnalyticsNotFoundError(f"Longest streak has not been found.")
 
 
+
 	@handle_analytics_repository_errors
 	def get_same_periodicity_type_habits(self):
+		"""
+		Groups habits by periodicity type and returns the count and a concatenated list of habit details for each group.
+
+		Returns:
+			list of tuples: Each tuple contains (periodicity_type, habit_count, habit_list).
+							- periodicity_type (str):  "daily" or "weekly".
+							- habit_count (int): Number of habits with that periodicity.
+							- habit_list (str): A comma-separated string of "habit_id: habit_name" pairs.
+
+		Raises:
+			AnalyticsNotFoundError: If no habits are found.
+			IntegrityError: For database integrity-related errors.
+			AnalyticsRepositoryError: For other repository-level analytics errors.
+			Exception: For any other unexpected errors.
+		"""
 		with self._db._connection.cursor() as cursor:
 			query = "SELECT habit_periodicity_type, COUNT(*) AS habit_count, GROUP_CONCAT(CONCAT(habit_id, ': ', habit_name) SEPARATOR ', ') AS habit_list FROM habits GROUP BY habit_periodicity_type ORDER BY habit_count DESC;"
 
@@ -146,8 +239,21 @@ class AnalyticsRepository:
 				raise AnalyticsNotFoundError(f"Same periodicity types have not been found.")
 
 
+
 	@handle_analytics_repository_errors
 	def get_currently_tracked_habits(self):
+		"""
+		Retrieves a list of habits that currently have an active streak (i.e., streak > 0).
+
+		Returns:
+			list of tuples: Each tuple contains (habit_id, habit_name, habit_streak, habit_periodicity_type).
+
+		Raises:
+			AnalyticsNotFoundError: If no tracked habits are found.
+			IntegrityError: For database integrity-related errors.
+			AnalyticsRepositoryError: For other repository-level analytics errors.
+			Exception: For any other unexpected errors.
+		"""
 		with self._db._connection.cursor() as cursor:
 			query = "SELECT habit_id, habit_name, habit_streak, habit_periodicity_type FROM habits WHERE habit_streak > 0;"
 
@@ -159,8 +265,25 @@ class AnalyticsRepository:
 			else:
 				raise AnalyticsNotFoundError(f"Currently tracked habits are not found.")
 
+
+
 	@handle_analytics_repository_errors
 	def longest_streak_for_habit(self, habit_id):
+		"""
+		Retrieves the progress entry with the highest streak for the specified habit.
+
+		Args:
+			habit_id (int): The unique identifier of the habit.
+
+		Returns:
+			list of tuples: Each tuple represents a progress record sorted by the highest streak first.
+
+		Raises:
+			AnalyticsNotFoundError: If no progress entries are found for the given habit.
+			IntegrityError: For database integrity-related errors.
+			AnalyticsRepositoryError: For other repository-level analytics errors.
+			Exception: For any other unexpected errors.
+		"""
 		with self._db._connection.cursor() as cursor:
 			query = "SELECT p.* FROM progresses p JOIN goals g ON p.goal_id_id = g.goal_id WHERE g.habit_id_id = %s ORDER BY p.current_streak DESC LIMIT 1;"
 			cursor.execute(query, (habit_id, ))
--- apps/analytics/services/analytics_service.py
+++ apps/analytics/services/analytics_service.py
@@ -47,16 +47,55 @@ class AnalyticsService:
 		
 		if times_completed and (times_completed < 0 or times_completed > 365):
 			raise ValueError("times_completed must be between 0 and 365.")
-		
+
+
+
 	@handle_analytics_service_exceptions
 	def create_analytics(self, habit_id, times_completed, streak_length, last_completed_at=None):
+		"""
+		Creates an analytics entry for a given habit.
+
+		Args:
+			habit_id (int): The unique identifier of the habit.
+			times_completed (int): The total number of completions for the habit.
+			streak_length (int): The current streak length of the habit.
+			last_completed_at (datetime, optional): The last time the habit was completed. Defaults to None.
+
+		Returns:
+			dict: The newly created analytics record including its 'analytics_id'.
+
+		Logs and Raises:
+			AnalyticsNotFoundError: If the habit cannot be found or is invalid.
+			IntegrityError: For database integrity-related errors.
+			Exception: For other unexpected errors in the analytics service.
+		"""
 		validated_habit_id = self._habit_service.validate_a_habit(habit_id)
 		analytics_entity = self._repository.create_analytics(times_completed, streak_length, validated_habit_id, last_completed_at=last_completed_at)
 		
 		return analytics_entity
-		
+
+
+
 	@handle_analytics_service_exceptions
 	def update_analytics(self, habit_id, times_completed=None, streak_length=None, last_completed_at=None):
+		"""
+		Updates an existing analytics record tied to a given habit.
+
+		Args:
+			habit_id (int): The unique identifier of the habit.
+			times_completed (int, optional): Updated number of habit completions. Defaults to None.
+			streak_length (int, optional): Updated streak length of the habit. Defaults to None.
+			last_completed_at (datetime, optional): Updated datetime for the last completion. Defaults to None.
+
+		Returns:
+			int: The number of rows updated (typically 1 on success).
+
+		Logs and Raises:
+			AnalyticsNotFoundError: If there's no analytics entry for the given habit.
+			IntegrityError: For database integrity-related issues.
+			Exception: For any other unexpected errors in analytics service.
+		"""
+		
 		#get analytics id, if there is none this will raise an error
 		analytics_id = self._repository.get_analytics_id(habit_id)
 
@@ -68,14 +107,46 @@ class AnalyticsService:
 		return updated_rows
 		
 
+
 	@handle_analytics_service_exceptions
 	def get_analytics_id(self, habit_id):
+		"""
+		Retrieves the analytics ID for the specified habit.
+
+		Args:
+			habit_id (int): The unique identifier of the habit.
+
+		Returns:
+			int: The ID of the analytics record tied to the provided habit_id.
+
+		Logs and Raises:
+			AnalyticsNotFoundError: If no analytics record is found for the habit.
+			IntegrityError: For database integrity-related issues.
+			Exception: For any other unexpected errors in analytics service.
+		"""
 		#should we validate whether habit id exists first?
 		analytics_id = self._repository.get_analytics_id(habit_id)
 		return analytics_id
 
+
+
 	@handle_analytics_service_exceptions
 	def delete_analytics(self, habit_id=None, analytics_id=None):
+		"""
+		Deletes an analytics record either by direct analytics ID or by habit ID.
+
+		Args:
+			habit_id (int, optional): The habit's unique identifier. Defaults to None.
+			analytics_id (int, optional): The analytics record's unique identifier. Defaults to None.
+
+		Returns:
+			int: The number of rows deleted (usually 1 on success).
+
+		Logs and Raises:
+			ValueError: If neither habit_id nor analytics_id is provided when needed.
+			AnalyticsNotFoundError: If the record to be deleted is not found.
+			IntegrityError: For database integrity-related issues.
+		"""
 		self.validate_analytics('delete', habit_id=habit_id, analytics_id=analytics_id)
 
 		if habit_id and not analytics_id:
@@ -84,22 +155,81 @@ class AnalyticsService:
 		deleted_rows = self._repository.delete_analytics(analytics_id)
 		return deleted_rows
 
+
+
 	@handle_analytics_service_exceptions
 	def calculate_longest_streak(self):
+		"""
+		Returns the habit with the current longest streak from the database.
+
+		Returns:
+			tuple: (habit_id, habit_name, habit_streak) representing the habit with the maximum streak.
+
+		Logs and Raises:
+			AnalyticsNotFoundError: If no longest streak is found.
+			IntegrityError: For database integrity-related issues.
+			Exception: For any other unexpected errors in analytics service.
+		"""
 		result = self._repository.calculate_longest_streak()
 		return result
 
+
+
 	@handle_analytics_service_exceptions
 	def get_same_periodicity_type_habits(self):
+		"""
+		Groups and retrieves habits by periodicity type.
+
+		Returns:
+			list of tuples: Each tuple contains:
+							(periodicity_type, habit_count, habit_list).
+							- periodicity_type (str): 'daily' or 'weekly', etc.
+							- habit_count (int): Number of habits in that group.
+							- habit_list (str): Concatenated details of habits.
+
+		Raises:
+			AnalyticsNotFoundError: If no habits are found in any group.
+			IntegrityError: For database integrity-related issues.
+			Exception: For other unexpected errors in analytics service.
+		"""
 		result = self._repository.get_same_periodicity_type_habits()
 		return result
 
+
+
 	@handle_analytics_service_exceptions
 	def get_currently_tracked_habits(self):
+		"""
+		Retrieves habits that currently have a streak above zero.
+
+		Returns:
+			list of tuples: E.g., [(habit_id, habit_name, streak, periodicity_type), ...]
+
+		Raises:
+			AnalyticsNotFoundError: If there are no habits currently being tracked.
+			IntegrityError: For database integrity-related issues.
+			Exception: For any other unexpected errors in analytics service.
+		"""
 		result = self._repository.get_currently_tracked_habits()
 		return result
 
+
+
 	@handle_analytics_service_exceptions
 	def longest_streak_for_habit(self, habit_id):
+		"""
+		Retrieves the single highest streak entry from 'progresses' for a given habit.
+
+		Args:
+			habit_id (int): The unique identifier of the habit.
+
+		Returns:
+			list of tuples: The fetched progress records ordered by largest streak first.
+
+		Raises:
+			AnalyticsNotFoundError: If no progress data is found for the habit.
+			IntegrityError: For database integrity-related issues.
+			Exception: For other unexpected errors in analytics service.
+		"""
 		result = self._repository.longest_streak_for_habit(habit_id)
 		return result
